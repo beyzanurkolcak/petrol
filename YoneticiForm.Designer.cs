5603406f6cff4e54c0f530ddc837cc5f938b5da6
Beyzanur Kolçak <165806344+beyzanurkolcak@users.noreply.github.com>
2024-12-12 23:36:48 +0300

    Add files via upload

---
+++ YoneticiForm.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace petrolOtomasyon
+{
+    partial class YoneticiForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnPersonelEkle = new System.Windows.Forms.Button();
+            this.txtIsim = new System.Windows.Forms.TextBox();
+            this.txtSoyisim = new System.Windows.Forms.TextBox();
+            this.txtPozisyon = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxBranches = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.btnOnayla = new System.Windows.Forms.Button();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.lstPersonel = new System.Windows.Forms.ListBox();
+            this.dataSet1 = new petrolOtomasyon.DataSet1();
+            this.dataSet1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 227);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(742, 170);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnPersonelEkle
+            // 
+            this.btnPersonelEkle.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnPersonelEkle.Location = new System.Drawing.Point(434, 105);
+            this.btnPersonelEkle.Name = "btnPersonelEkle";
+            this.btnPersonelEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnPersonelEkle.TabIndex = 1;
+            this.btnPersonelEkle.Text = "EKLE";
+            this.btnPersonelEkle.UseVisualStyleBackColor = false;
+            this.btnPersonelEkle.Click += new System.EventHandler(this.btnPersonelEkle_Click);
+            // 
+            // txtIsim
+            // 
+            this.txtIsim.Location = new System.Drawing.Point(32, 105);
+            this.txtIsim.Name = "txtIsim";
+            this.txtIsim.Size = new System.Drawing.Size(100, 22);
+            this.txtIsim.TabIndex = 2;
+            // 
+            // txtSoyisim
+            // 
+            this.txtSoyisim.Location = new System.Drawing.Point(167, 105);
+            this.txtSoyisim.Name = "txtSoyisim";
+            this.txtSoyisim.Size = new System.Drawing.Size(100, 22);
+            this.txtSoyisim.TabIndex = 3;
+            // 
+            // txtPozisyon
+            // 
+            this.txtPozisyon.Location = new System.Drawing.Point(317, 105);
+            this.txtPozisyon.Name = "txtPozisyon";
+            this.txtPozisyon.Size = new System.Drawing.Size(100, 22);
+            this.txtPozisyon.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "AD";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "SOYAD";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(352, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "POZİSYON";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(192, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Personel Ekleme";
+            // 
+            // comboBoxBranches
+            // 
+            this.comboBoxBranches.FormattingEnabled = true;
+            this.comboBoxBranches.Location = new System.Drawing.Point(1129, 230);
+            this.comboBoxBranches.Name = "comboBoxBranches";
+            this.comboBoxBranches.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxBranches.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(960, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Talep Oluştur";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(843, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Yakıt Talebi";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(846, 256);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView2.TabIndex = 12;
+            // 
+            // btnOnayla
+            // 
+            this.btnOnayla.BackColor = System.Drawing.Color.LawnGreen;
+            this.btnOnayla.Location = new System.Drawing.Point(1143, 291);
+            this.btnOnayla.Name = "btnOnayla";
+            this.btnOnayla.Size = new System.Drawing.Size(75, 23);
+            this.btnOnayla.TabIndex = 13;
+            this.btnOnayla.Text = "ONAY";
+            this.btnOnayla.UseVisualStyleBackColor = false;
+            this.btnOnayla.Click += new System.EventHandler(this.btnOnayla_Click);
+            // 
+            // btnRed
+            // 
+            this.btnRed.BackColor = System.Drawing.Color.Red;
+            this.btnRed.Location = new System.Drawing.Point(1143, 332);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(75, 23);
+            this.btnRed.TabIndex = 14;
+            this.btnRed.Text = "RED";
+            this.btnRed.UseVisualStyleBackColor = false;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // lstPersonel
+            // 
+            this.lstPersonel.FormattingEnabled = true;
+            this.lstPersonel.ItemHeight = 16;
+            this.lstPersonel.Location = new System.Drawing.Point(624, 30);
+            this.lstPersonel.Name = "lstPersonel";
+            this.lstPersonel.Size = new System.Drawing.Size(243, 148);
+            this.lstPersonel.TabIndex = 15;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataSet1BindingSource
+            // 
+            this.dataSet1BindingSource.DataSource = this.dataSet1;
+            this.dataSet1BindingSource.Position = 0;
+            // 
+            // YoneticiForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1636, 750);
+            this.Controls.Add(this.lstPersonel);
+            this.Controls.Add(this.btnRed);
+            this.Controls.Add(this.btnOnayla);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBoxBranches);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPozisyon);
+            this.Controls.Add(this.txtSoyisim);
+            this.Controls.Add(this.txtIsim);
+            this.Controls.Add(this.btnPersonelEkle);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "YoneticiForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnPersonelEkle;
+        private System.Windows.Forms.TextBox txtIsim;
+        private System.Windows.Forms.TextBox txtSoyisim;
+        private System.Windows.Forms.TextBox txtPozisyon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxBranches;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button btnOnayla;
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.ListBox lstPersonel;
+        private System.Windows.Forms.BindingSource dataSet1BindingSource;
+        private DataSet1 dataSet1;
+    }
+}
+
---
+++ YoneticiForm.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace petrolOtomasyon
+{
+    public partial class YoneticiForm : Form
+    {
+        SqlConnection connection = new SqlConnection(@"Server=BEYZA;Database=petrolOtomasyon;Trusted_Connection=True;");
+
+        public YoneticiForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Veritabanı bağlantısı için bağlantı dizesi
+            string connectionString = "Server=localhost; Database=petrolOtomasyon; Integrated Security=True;";
+
+            // İlk SQL sorgusu: Branch bilgilerini almak
+            string branchQuery = "SELECT BranchName, Location, City FROM dbo.Branch";
+
+            try
+            {
+                // SQL bağlantısını oluşturuyoruz ve açıyoruz
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Branch bilgilerini almak için komut oluşturuyoruz
+                    SqlCommand branchCommand = new SqlCommand(branchQuery, connection);
+
+                    // Veriyi okuyoruz
+                    SqlDataReader reader = branchCommand.ExecuteReader();
+
+                    // ComboBox'ı temizliyoruz
+                    comboBoxBranches.Items.Clear();
+
+                    // Veritabanından veri okuma ve ComboBox'a ekleme
+                    while (reader.Read())
+                    {
+                        string branchName = reader["BranchName"].ToString();
+                        string location = reader["Location"].ToString();
+                        string city = reader["City"].ToString();
+
+                        // ComboBox'a yeni item ekliyoruz
+                        comboBoxBranches.Items.Add($"{branchName} - {location}, {city}");
+                    }
+
+                    // Reader'ı kapatıyoruz
+                    reader.Close();
+
+                    // İkinci SQL sorgusu: Employee bilgilerini almak
+                    string employeeQuery = "SELECT Name, Position, BranchID FROM dbo.Employee";
+
+                    // Employee bilgilerini almak için komut oluşturuyoruz
+                    SqlCommand employeeCommand = new SqlCommand(employeeQuery, connection);
+
+                    // Veriyi okuyoruz
+                    reader = employeeCommand.ExecuteReader();
+
+                    // lstPersonel listesine personelleri ekliyoruz
+                    while (reader.Read())
+                    {
+                        string name = reader["Name"].ToString();
+                        string position = reader["Position"].ToString();
+                        int branchId = (int)reader["BranchID"]; // BranchID alınıyor
+
+                        // Personeli listeye ekliyoruz
+                        lstPersonel.Items.Add($"{name} - {position} (Şube {branchId})");
+                    }
+
+                    // Reader'ı kapatıyoruz
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Hata mesajını göster
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand("SELECT * FROM Branch", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+
+        private void btnPersonelEkle_Click(object sender, EventArgs e)
+        {
+            string isim = txtIsim.Text;
+            string soyisim = txtSoyisim.Text;
+            string pozisyon = txtPozisyon.Text;
+            string connectionString = "Server=BEYZA;Database=petrolOtomasyonu;Trusted_Connection=True;";
+
+            lstPersonel.Items.Add(isim + " " + soyisim + " - " + pozisyon);
+        }
+
+        private void btnOnayla_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var seciliSatir = dataGridView1.SelectedRows[0];
+                string talepDetayi = seciliSatir.Cells["TalepDetayi"].Value.ToString();
+
+                // Yakıt talebini veritabanına onaylama işlemi yapılacak
+
+                MessageBox.Show("Talep onaylandı: " + talepDetayi);
+            }
+            else
+            {
+                MessageBox.Show("Lütfen onaylamak için bir talep seçin.");
+            }
+        }
+
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var seciliSatir = dataGridView1.SelectedRows[0];
+                string talepDetayi = seciliSatir.Cells["TalepDetayi"].Value.ToString();
+
+                // Yakıt talebini veritabanına reddetme işlemi yapılacak
+
+                MessageBox.Show("Talep reddedildi: " + talepDetayi);
+            }
+            else
+            {
+                MessageBox.Show("Lütfen reddetmek için bir talep seçin.");
+            }
+        }
+    }
+}
